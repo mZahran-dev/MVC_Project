5a6a8fa145ffac7acfe865cc6f8aabadd0280a2a
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-06 01:29:01 +0300

    Add project files.

---
+++ MVC_Project_DAL/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_Project_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project_DAL.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-M4KTLBQ; Database = MVC_Project; Trusted_Connection = True; Encrypt = false; MultipleActiveResultSets = True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
---
+++ MVC_Project_DAL/Data/Configuration/DepartmentConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MVC_Project_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project_DAL.Data.Configuration
+{
+    internal class DepartmentConfiguration : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            //Fluent APIS
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}
