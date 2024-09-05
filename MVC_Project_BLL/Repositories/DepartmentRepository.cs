0cf8b6f4c39fb5dc5b7a425bd88d341f3a8883cb
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-06 02:10:46 +0300

    feat : implementing Repositories Design pattern

---
+++ MVC_Project_BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,38 @@
+﻿using MVC_Project_BLL.Interfaces;
+using MVC_Project_DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC_Project_BLL.Repositories
+{
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        public int delete(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Department> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Department GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int insert(Department department)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int update(Department department)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
