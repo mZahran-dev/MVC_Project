0bca626036fe11dbc06dd2cb1a04a804ef18c677
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-11 23:22:35 +0300

    Add Create & Details Action

--- MVC_Project/Controllers/DepartmentController.cs
+++ MVC_Project/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using MVC_Project_BLL.Interfaces;
+using MVC_Project_DAL.Models;
 
 namespace MVC_Project_PL.Controllers
 {
@@ -15,6 +17,40 @@ namespace MVC_Project_PL.Controllers
             // GetAll calling
             var departmentIndex =  departmentRepository.GetAll();
             return View(departmentIndex);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department) 
+        {
+            if (ModelState.IsValid)
+            {
+                var count = departmentRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(department);
+        }
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+            var department = departmentRepository.GetById(id.Value);
+            if (department == null)
+            {
+                return NotFound();
+            } 
+            return View(department);
         }
     }
 }
