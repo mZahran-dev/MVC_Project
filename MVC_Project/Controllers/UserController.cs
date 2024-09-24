5ef4718fae3dcd813bff5c7fab5b1b14dfb5470e
Mahmoud Zahran <mi4654110@gmail.com>
2024-09-24 03:12:35 +0300

    Another Approach for Index Problem of UserController

--- MVC_Project/Controllers/UserController.cs
+++ MVC_Project/Controllers/UserController.cs
@@ -20,40 +20,91 @@ namespace MVC_Project_PL.Controllers
 			_userManager = userManager;
 			_roleManager = roleManager;
 		}
+		//public async Task<IActionResult> Index(string email)
+		//{
+		//	if (string.IsNullOrEmpty(email))
+		//	{
+		//		var users = await _userManager.Users.Select(U => new UserViewModel
+		//		{
+		//			Id = U.Id,
+		//			Fname = U.FName,
+		//			Lname = U.LName,
+		//			phoneNumber = U.PhoneNumber,
+		//			Email = U.Email,
+		//			Roles = _userManager.GetRolesAsync(U).Result
+		//		}).ToListAsync();
+		//		return View(users);
+		//	}
+		//	else
+		//	{
+		//		var user = await _userManager.FindByIdAsync(email);
+		//		if (user != null)
+		//		{
+		//			var mappeduser = new UserViewModel
+		//			{
+		//				Id = user.Id,
+		//				Fname = user.FName,
+		//				Lname = user.LName,
+		//				phoneNumber = user.PhoneNumber,
+		//				Email = user.Email,
+		//				Roles = await _userManager.GetRolesAsync(user),
+		//			};
+		//			return View(new List<UserViewModel> { mappeduser });
+		//		}
+		//	}
+
+		//	return View(Enumerable.Empty<UserViewModel>());
+		//}
+
+
+
+
+
 		public async Task<IActionResult> Index(string email)
 		{
 			if (string.IsNullOrEmpty(email))
 			{
-				var users = await _userManager.Users.Select(U => new UserViewModel
+				// Fetch users first, without using async in Select
+				var users = await _userManager.Users.ToListAsync();
+
+				// Now asynchronously fetch roles for each user
+				var userViewModels = new List<UserViewModel>();
+				foreach (var user in users)
 				{
-					Id = U.Id,
-					Fname = U.FName,
-					Lname = U.LName,
-					phoneNumber = U.PhoneNumber,
-					Email = U.Email,
-					Roles = _userManager.GetRolesAsync(U).Result
-				}).ToListAsync();
-				return View(users);
-			}
-			else
-			{
-				var user = await _userManager.FindByIdAsync(email);
-				if (user != null)
-				{
-					var mappeduser = new UserViewModel
+					var userViewModel = new UserViewModel
 					{
 						Id = user.Id,
 						Fname = user.FName,
 						Lname = user.LName,
 						phoneNumber = user.PhoneNumber,
 						Email = user.Email,
-						Roles = await _userManager.GetRolesAsync(user),
+						Roles = await _userManager.GetRolesAsync(user)  // Use await properly here
 					};
-					return View(new List<UserViewModel> { mappeduser });
+					userViewModels.Add(userViewModel);
+				}
+
+				return View(userViewModels);
+			}
+			else
+			{
+				var user = await _userManager.FindByIdAsync(email);
+				if (user != null)
+				{
+					var mappedUser = new UserViewModel
+					{
+						Id = user.Id,
+						Fname = user.FName,
+						Lname = user.LName,
+						phoneNumber = user.PhoneNumber,
+						Email = user.Email,
+						Roles = await _userManager.GetRolesAsync(user),  // Use await
+					};
+					return View(new List<UserViewModel> { mappedUser });
 				}
 			}
 
 			return View(Enumerable.Empty<UserViewModel>());
 		}
+
 	}
 }
